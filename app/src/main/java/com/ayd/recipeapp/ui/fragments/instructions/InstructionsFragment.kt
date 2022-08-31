292b6aab122ed07ff342432cab31ccc8540d8dfa
Atilla Aydemir <atilla-734@hotmail.com>
2022-08-31 22:00:24 +0300

    "added Instruction fragment"

--- app/src/main/java/com/ayd/recipeapp/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/ayd/recipeapp/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,19 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ayd.recipeapp.R
+import android.webkit.WebViewClient
+import com.ayd.recipeapp.databinding.FragmentInstructionsBinding
+import com.ayd.recipeapp.model.Result
+import com.ayd.recipeapp.util.Constants.Companion.RECIPE_RESULT_KEY
 
 
 class InstructionsFragment : Fragment() {
 
-
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    ): View {
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        binding.instrucionWebView.webViewClient = object : WebViewClient(){}
+        val webSiteUrl: String = myBundle!!.sourceUrl
+        binding.instrucionWebView.loadUrl(webSiteUrl)
+
+
+        return binding.root
     }
 
 
